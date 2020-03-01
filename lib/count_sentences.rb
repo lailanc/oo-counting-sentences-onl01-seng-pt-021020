976d14ec54d96bed4684b8600c30062f389dd032
Laila Campbell <lailacampbell96@gmail.com>
2020-03-01 17:02:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,8 @@ require 'pry'
 class String
 
   def sentence?
-    
+  #"returns true if the string that you are calling this method on ends in a period."
+    self.end_with?(".") 
   end
 
   def question?
